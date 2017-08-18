4fd5c090db0f5d4105e36443e4815cac4f396dae
KlimKREMEN <papachiv.roman@gmail.com>
2017-08-18 19:01:21 +0300

    second commit

---
+++ app/js/main.js
@@ -0,0 +1,194 @@
+function setSaHeight(saVideo) {
+    saVideo.find('.sa-video__item').css({
+        height: saVideo.find('.owl-item').width(),
+    });
+}
+
+$('document').ready(function(){
+
+    // Single blog page - slider
+    // $("#single-blog-page").owlCarousel({
+    //     items: 1,
+    //     loop: true,
+    //     lazyLoad: true,
+    //     autoplay: true,
+    //     autoplayHoverPause: true,
+    //     nav: true,
+    //     navText: [
+    //         "<img src='/img/owl-prev.png'>",
+    //         "<img src='/img/owl-next.png'>"
+    //     ]
+    // });
+    //
+    // // Athlete list - slider
+    // $("#athletes-slider").owlCarousel({
+    //     items: 1,
+    //     loop: true,
+    //     lazyLoad: true,
+    //     autoplay: true,
+    //     autoplayHoverPause: true,
+    //     nav: true,
+    //     navText: [
+    //         "<img src='/img/owl-prev.png'>",
+    //         "<img src='/img/owl-next.png'>"
+    //     ]
+    // });
+    //
+    // // Athlete list - slider
+    // $("#sa-video-slider").owlCarousel({
+    //     autoplayHoverPause: true,
+    //     items: 3,
+    //     autoplay: true,
+    //     loop: true,
+    //     margin: 0,
+    //     video: true,
+    //     lazyLoad: false,
+    //     nav: true,
+    //     navText: [
+    //         "<i class='fa fa-chevron-left' aria-hidden='true'></i>",
+    //         "<i class='fa fa-chevron-right' aria-hidden='true'></i>"
+    //     ]
+    // });
+    //
+    // // Single athlete career history
+    // $("#sa-carier-history").owlCarousel({
+    //     items: 4,
+    //     loop: true,
+    //     lazyLoad: true,
+    //     autoplay: false,
+    //     autoplayHoverPause: true,
+    //     nav: true,
+    //     navText: [
+    //         "<img src='/img/owl-prev.png'>",
+    //         "<img src='/img/owl-next.png'>"
+    //     ]
+    // });
+    //
+    // // Single athlete ACHIEVEMENTS
+    // $("#sa-achievements").owlCarousel({
+    //     items: 4,
+    //     loop: true,
+    //     lazyLoad: true,
+    //     autoplay: false,
+    //     autoplayHoverPause: true,
+    //     nav: true,
+    //     navText: [
+    //         "<img src='/img/owl-prev.png'>",
+    //         "<img src='/img/owl-next.png'>"
+    //     ]
+    // });
+    //
+    // // Single athlete SPONSORS
+    // $("#sa-sponsors").owlCarousel({
+    //     items: 4,
+    //     loop: true,
+    //     lazyLoad: true,
+    //     margin: 30,
+    //     autoplay: true,
+    //     autoplayHoverPause: true,
+    //     nav: true,
+    //     navText: [
+    //         "<img src='/img/owl-prev.png'>",
+    //         "<img src='/img/owl-next.png'>"
+    //     ]
+    // });
+    //
+    // setSaHeight($("#sa-video-slider"));
+    //
+    // // single-athlete page social media progress
+    // $('.social-media__progres-item').each(function(){
+    //     var smProgress = $(this).data('progres');
+    //     $(this).css({
+    //         width: smProgress + '%',
+    //     });
+    // });
+    //
+    // // Single athlete video player
+    // var videoPlayer;
+    // $('.show-modal').click(function(){
+    //     var modalName = $(this).data('modal');
+    //     if (modalName == 'youtube-modal') {
+    //         var videoID = $(this).data('video');
+    //         $('.youtube-set-video').attr('data-video-id', videoID);
+    //     }
+    //     $('#'+modalName).modal('show');
+    //     if (modalName == 'youtube-modal') {
+    //         videoPlayer = plyr.setup(document.querySelector('.js-player'));
+    //     }
+    // });
+    //
+    // $('#youtube-modal').on('hide.bs.modal', function (e) {
+    //     videoPlayer[0].destroy();
+    //     $('.youtube-set-video').removeAttr('data-video-id');
+    // });
+
+
+
+    $(document).ready(function () {
+        var pie1 = $('.pie-1'),
+            pie2 = $('.pie-2'),
+            pie3 = $('.pie-3');
+        progressBarUpdate(95, 100, pie1);
+        progressBarUpdate(47, 100, pie2);
+        progressBarUpdate(33, 100, pie3);
+    });
+
+
+    var pie1 = $('.pie-1'),
+        pie2 = $('.pie-2'),
+        pie3 = $('.pie-3');
+    progressBarUpdate(11, 100, pie1);
+    progressBarUpdate(22, 100, pie2);
+    progressBarUpdate(33, 100, pie3);
+
+
+    function rotate(element, degree) {
+        element.css({
+            '-webkit-transform': 'rotate(' + degree + 'deg)',
+            '-moz-transform': 'rotate(' + degree + 'deg)',
+            '-ms-transform': 'rotate(' + degree + 'deg)',
+            '-o-transform': 'rotate(' + degree + 'deg)',
+            'transform': 'rotate(' + degree + 'deg)',
+            'zoom': 1
+        });
+    }
+
+    function progressBarUpdate(x, outOf, elem) {
+        var firstHalfAngle = 180;
+        var secondHalfAngle = 0;
+
+        // caluclate the angle
+        var drawAngle = x / outOf * 360;
+
+        // calculate the angle to be displayed if each half
+        if (drawAngle <= 180) {
+            firstHalfAngle = drawAngle;
+        } else {
+            secondHalfAngle = drawAngle - 180;
+        }
+
+        // set the transition
+        rotate(elem.find(".slice1"), firstHalfAngle);
+        rotate(elem.find(".slice2"), secondHalfAngle);
+
+        // set the values on the text
+        elem.find(".status").html(x + "<span>%</span>");
+    }
+
+    $(".anchor").on("click", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        // top = +top -100;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+});
+
+$(window).resize(function(){
+    setSaHeight($("#sa-video-slider"));
+});
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var gulp            = require('gulp'),
+    sass            = require('gulp-sass'),
+    browserSync     = require('browser-sync'),
+    concat          = require('gulp-concat'),
+    uglify          = require('gulp-uglify'),
+    rename          = require('gulp-rename'),
+    cssnano         = require('gulp-cssnano'),
+    del             = require('del'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    spritesmith     = require('gulp.spritesmith'),
+    //phantomjssmith  = require('phantomjssmith');,
+    merge           = require('merge-stream'),
+    buffer          = require('vinyl-buffer'),
+    imagemin        = require('gulp-imagemin'),
+    csso            = require('gulp-csso'),
+    sourcemaps      = require('gulp-sourcemaps');
+
+gulp.task('sass', function(){
+    return gulp.src('app/sass/**/*.scss')
+        //.pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: false }))
+        //.pipe(sourcemaps.write('../css', {addComment: false}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync',function(){
+    browserSync({
+        proxy: "HSstatic",
+        notify: false,
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        // 'app/libs/modernizr.js',
+        'app/libs/jQuery/dist/jquery.min.js',
+        'app/libs/bootstrap/dist/js/bootstrap.min.js',
+        'app/libs/owlcarousel2/dist/owl.carousel.min.js',
+        'app/libs/plyr/dist/plyr.js',
+        'app/libs/radialIndicator/radialIndicator.min.js',
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/libs.css')
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('sprite', function () {
+    // Generate our spritesheet
+    var spriteData = gulp.src('app/img/icons/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: '_sprite.scss'
+        }));
+
+    // Pipe image stream through image optimizer and onto disk
+    var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+    //     .pipe(buffer())
+    //     .pipe(imagemin())
+        .pipe(gulp.dest('app/img'));
+
+    // Pipe CSS stream through CSS optimizer and onto disk
+    var cssStream = spriteData.css
+        //.pipe(csso())
+        .pipe(gulp.dest('app/sass/'));
+
+    // Return a merged stream to handle both `end` events
+    return merge(imgStream, cssStream);
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+    gulp.watch('app/sass/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// ----------------------Дефолтный таск gulp --------------------------------
+
+gulp.task('default', ['watch']);
